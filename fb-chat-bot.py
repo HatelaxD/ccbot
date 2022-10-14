91de9418ba14489af4b6c293e03730d839743d92
HatelaxD <115532867+HatelaxD@users.noreply.github.com>
2022-10-14 12:44:45 +0500

    Add files via upload

--- fb-chat-bot.py
+++ fb-chat-bot.py
@@ -671,9 +671,9 @@ class ChatBot(Client):
 cookies = {
     "sb": "xasyYmAoy1tRpMGYvLxgkHBF",
     "fr": "0NxayJuewRHQ30OX3.AWVJwIYNh0Tt8AJv6kSwDamhkoM.BiMrVd.Iu.AAA.0.0.BiMtVZ.AWXMVaiHrpQ",
-    "c_user": "100055065882693",
+    "c_user": "",
     "datr": "xasyYs51GC0Lq5H5lvXTl5zA",
-    "xs": "39%3AdSbyORZ6oSehsA%3A2%3A1665594085%3A-1%3A6126%3A%3AAcW8qwm-rIDBcGVJBSdBE4FR5Em-eduUhi8xMqDSzg"
+    "xs": ""
 }
 
 
